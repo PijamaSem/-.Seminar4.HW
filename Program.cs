0daef8a16b4a724dd7b66fff045912934119c79b
Pijama_sem <example@example.com>
2022-11-13 02:15:46 +0300

    task 27

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿//Task 25. Write program, which get two digit (A) and (B) and return result  digit (A) in degree (B)
+﻿//Task 25. Write program, which get two digit (A) and (B) and return result  digit (A) in degree natural (B)
+/*
 int NaturalNumber(int num) // проверка второй числа, натуральное или нет
     {
         if (num > 0) //ноль не натуральное число проверка "0" не нужна
@@ -28,3 +29,43 @@ int numA  = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"thanks, now put natural number (B):");   // аргумент (В) должен быть натуральным 1,2,3... нужна проверка
 int numB  = Convert.ToInt32(Console.ReadLine());
 DigitInDegree(numA, NaturalNumber(numB));  
+*/
+// Task 27. Write program, which get number and return sum of digit of this number (123=>1+2+3=6)
+
+int Decimal(int degree)
+    {
+    int result = 1;
+     for(int i=1; i<=degree ; i++)
+     {
+        result*=10;
+     }
+     return  result;
+    }
+int CountOfDigits(int num)
+
+    {
+    int count1 = 0;
+    while (num >0  )
+        {
+        count1++;
+        num = num /10;
+    }
+    return count1;
+    }
+int SumOfDigits(int num, int digits)
+    {
+    int sum=0;
+    for (int i=digits-1; i > -1; i--) // 123456 digit 6 1=123456/100000
+    {
+    sum+=num/Decimal(i);
+    num = num%Decimal(i);
+    //Console.WriteLine($"-{sum}-{num}-{i}-{digits}-{Decimal(i)}");
+    }
+    return sum;
+    }
+Console.WriteLine($"Dear, pls put number (A):");  // аргумент (А) 
+int number  = Convert.ToInt32(Console.ReadLine());
+int numberDigits = CountOfDigits(number);
+int sumDigits = SumOfDigits(number, numberDigits);
+//SumOfDigits (number, numberDigits);
+Console.WriteLine($"your number have {numberDigits} digit(s). Sum of it is: {sumDigits}");
